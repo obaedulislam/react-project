44498cef20a594bcf2a10b23a26ed816a41d6368
obaed <obaedul@redlimesolutions.com>
2024-04-05 09:49:17 +0600

    done basic modifications

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Person></Person>
+    </div>
+  );
+}
+
+function Person() {
+  return <h1>Mohammad Obaedul Islam</h1>;
 }
 
 export default App;
